c4589bc5a2355a22138329415512438ec5cebcb6
Ace <ace.github@gmail.com>
2015-06-03 14:16:36 -0700

    Basic cipher added

--- index.js
+++ index.js
@@ -7,6 +7,26 @@
     var io = require('socket.io')(http);
     var port = 5000;
 
+    var cipher = {
+        'a' : '!',
+        'b' : 'z',
+        'c' : 'K',
+        'd' : '0'
+    };
+
+    var translate = function translate(input) {
+        var len = input.length;
+        var output = [];
+        for (var i=0; i < len; i++) {
+            if (input[i] in cipher) {
+                output.push(cipher[input[i]]);
+            } else {
+            output.push(input[i]);
+            }
+        }
+        return output.join('');
+    };
+
     app.get('/', function(req, res) {
         res.sendFile(__dirname + '/index.html');
     });
@@ -15,7 +35,7 @@
 
     io.on('connection', function(socket) {
         socket.on('chat message', function (msg) {
-            io.emit('chat message', msg);
+            io.emit('chat message', translate(msg));
         });
     });
 
