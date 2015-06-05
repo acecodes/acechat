d1afd3ee8569d953b078b336c0d4d224453c5aa9
Ace <ace.github@gmail.com>
2015-06-04 18:57:15 -0700

    Added disconnect and connect messages to chat window

--- index.js
+++ index.js
@@ -7,22 +7,23 @@
     var io = require('socket.io')(http);
     var port = 5000;
 
+    // This will obviously need to be refined...
     var cipher = {
-        'a' : '!',
-        'A' : 's',
-        'b' : 'z',
-        'c' : 'K',
-        'd' : '0'
+        'a': '!',
+        'A': 's',
+        'b': 'z',
+        'c': 'K',
+        'd': '0'
     };
 
     var translate = function translate(input) {
         var len = input.length;
         var output = [];
-        for (var i=0; i < len; i++) {
+        for (var i = 0; i < len; i++) {
             if (input[i] in cipher) {
                 output.push(cipher[input[i]]);
             } else {
-            output.push(input[i]);
+                output.push(input[i]);
             }
         }
         return output.join('');
@@ -35,11 +36,15 @@
     app.use('/static', express.static(__dirname + '/static'));
 
     io.on('connection', function(socket) {
-        socket.on('chat message', function (msg) {
+        io.emit('chat message', 'User connected');
+        socket.on('chat message', function(msg) {
             io.emit('chat message', translate(msg));
         });
-    });
+        socket.on('disconnect', function() {
+            io.emit('chat message', 'User disconnected');
+        });
 
+    });
     http.listen(port, function() {
         console.log('Listening on port', port);
     });
